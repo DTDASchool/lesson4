b54919b7955f0d035791ad243352808ce434359c
DTDA Games <51952304+DTDASchool@users.noreply.github.com>
2019-08-02 10:04:48 +0200

    Add files via upload

---
+++ Assets/script/mouvement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouvement : MonoBehaviour
+{
+	public float mouvSpeed;
+	public float force;
+	public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+	
+
+    // Update is called once per frame
+    void Update()
+    {
+        float tempV = Input.GetAxis ("Vertical");
+		float tempH = Input.GetAxis ("Horizontal");
+		//Debug.Log(transform.position);
+		
+		transform.position = new Vector3 (transform.position.x + tempH * Time.deltaTime * mouvSpeed, transform.position.y, transform.position.z + tempV * Time.deltaTime * mouvSpeed);
+           
+        if(Input.GetKeyDown("space"))
+		{
+			rb.AddForce(transform.up*force);
+		}
+    }
+}
